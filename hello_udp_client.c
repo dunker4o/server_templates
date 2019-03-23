72a31edd08193a3f2de4f068c73bf16967902c5b
BorkoG <borko_3g@mail.com>
2019-03-23 00:22:40 +0000

    Modified the client to take destination IP address as argument.

--- hello_udp_client.c
+++ hello_udp_client.c
@@ -13,12 +13,20 @@
 #define PORT 31415
 #define MAXINPUT 12
 
-int main(void) {
+
+int main(int argc, char* argv[]) {
 	int 				fd;
 	char 				buffer[MAXLINE];
 	char 				*hello = "Hello from the other side of the internet!\n";
 	struct sockaddr_in 	server_addr;
 	char 				str[MAXINPUT]; //Array for reading stdin input
+	char 				*destination;
+
+	if(argc==2){
+		destination = argv[1];
+	} else{
+		destination = "127.0.0.1";
+	}
 
 	// Create the socket
 	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
@@ -29,7 +37,7 @@ int main(void) {
 	//Fillout the server information
 	server_addr.sin_family 		= AF_INET;
 	server_addr.sin_port 		= htons(PORT);
-	server_addr.sin_addr.s_addr = INADDR_ANY; 
+	server_addr.sin_addr.s_addr = inet_addr("192.168.0.9"); 
 
 	// Send data to the server
 	while(fgets(str, MAXINPUT, stdin) != NULL){
